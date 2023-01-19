cc3fbbe033c1a26e21041bd3f0d301e81a40a4c1
oscarardila96 <oscarardila96@outlook.com>
2023-01-18 20:16:49 -0500

    added token verification

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,20 +3,24 @@ require("dotenv").config();
 
 const authMiddleware = (req, res, next) => {
   let { authorization: token } = req.headers;
-  token = token.replace("Bearer ", "")
+  token = token?.replace("Bearer ", "")
   console.log(token);
-  jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    { algorithms: "HS512" },
-    (err, decoded) => {
-      if (err) {
-        res.status(400).json({ error: "Invalid token", message: "Your token is not valid, please send a valid token" })
-      } else {
-        console.log(decoded)
-        next();
-      }
-    });
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { algorithms: "HS512" },
+      (err, decoded) => {
+        if (err) {
+          res.status(400).json({ error: "Invalid token", message: "Your token is not valid, please send a valid token" })
+        } else {
+          console.log(decoded)
+          next();
+        }
+      });
+  } else {
+    res.status(400).json({ error: "No token provided", message: "No se envió token de autenticación" });
+  }
 };
 
 module.exports = authMiddleware;
